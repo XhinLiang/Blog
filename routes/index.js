b0e21c288f5a8dba6d9cf5d890be3407054fed4c
xhinliang <xhinliang@gmail.com>
2016-01-03 22:04:49 +0800

    init

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+module.exports = function(app) {
+    app.get('/', function (req, res) {
+        res.render('index', {
+            title : 'Index'
+        });
+    });
+
+    app.get('/register', function (req, res) {
+        res.render('register', {
+            title : 'Register'
+        });
+    });
+
+    app.post('/register', function(req, res) {
+
+    });
+
+
+    app.get('/login', function (req, res) {
+        res.render('login', {
+            title : 'Login'
+        });
+    });
+
+    app.post('/login', function (req, res) {
+
+    });
+
+    app.get('/post', function (req, res) {
+        res.render('post', {
+            title: 'Post'
+        });
+    });
+
+
+    app.post('/post', function (req, res) {
+
+    });
+
+    app.get('/logout', function (req, res) {
+
+    });
+};
---
+++ settings.js
@@ -0,0 +1,7 @@
+module.exports = {
+    mongoUrl: 'mongodb://localhost',
+    cookieSecret: 'blog',
+    dbName: 'blog',
+    host: 'localhost',
+    port: 27017
+};
