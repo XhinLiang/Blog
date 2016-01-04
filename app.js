49b7d9e87ffc2fd181732aa508fcae94ed4a115b
xhinliang <xhinliang@gmail.com>
2016-01-04 12:57:49 +0800

    Register

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+//flash 是一个在 session 中用于存储信息的特定区域。信息写入 flash
+//，下一次显示完毕后即被清除。典型的应用是结合重定向的功能
+//，确保信息是提供给下一个被渲染的页面。
+var flash = require('connect-flash');
 var routes = require('./routes/index');
 var settings = require('./settings');
 var users = require('./routes/users');
@@ -17,23 +20,25 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(flash());
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 
 app.use(session({
-    secret: settings.cookieSecret,
-    key: settings.dbName,
-    cookie: {
-        maxAge: 1000*60*60*24*30
-    },
-    store: new MongoStore({
-        url: settings.mongoUrl
-    })
+  secret: settings.cookieSecret,
+  key: settings.dbName,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 30
+  },
+  store: new MongoStore({
+    url: settings.mongoUrl
+  })
 }));
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -44,9 +49,9 @@ routes(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handlers
@@ -54,23 +59,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
     });
+  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
+var crypto = require('crypto');
+var User = require('../models/user.js');
+
 module.exports = function(app) {
     app.get('/', function (req, res) {
         res.render('index', {
@@ -12,32 +15,69 @@ module.exports = function(app) {
     });
 
     app.post('/register', function(req, res) {
+        var password = req.body.password;
+        var passwordRepeat = req.body['password_re'];
 
-    });
+        if (password_re != password) {
+            req.flash('error', 'Password doesn\'t match.');
+            return res.redirect('/register');
+        }
 
+        var md5 = crypto.createHash('md5');
+        passwordMD5 = md5.update(password).digest('hex');
 
-    app.get('/login', function (req, res) {
-        res.render('login', {
-            title : 'Login'
+        var newUser = new User({
+            name: req.body.name,
+            password: passwordMD5,
+            email: req.body.email
+        });
+
+        User.get(newUser.name, function (err, user) {
+            if (err) {
+                req.flash('Error', err);
+                return res.redirect('/');
+            }
+            if (user) {
+                req.flash('Error', 'User name has been used.');
+                return res.redirect('/register');
+            }
+
+            newUser.save(function (err, user) {
+                if (err) {
+                    req.flash('Error', err);
+                    return res.redirect('/register');
+                }
+                req.session.user = user;
+                req.flash('Success', 'Register Success');
+                return res.redirect('/');
+            });
         });
     });
+});
 
-    app.post('/login', function (req, res) {
 
+app.get('/login', function (req, res) {
+    res.render('login', {
+        title : 'Login'
     });
+});
 
-    app.get('/post', function (req, res) {
-        res.render('post', {
-            title: 'Post'
-        });
+app.post('/login', function (req, res) {
+
+});
+
+app.get('/post', function (req, res) {
+    res.render('post', {
+        title: 'Post'
     });
+});
 
 
-    app.post('/post', function (req, res) {
+app.post('/post', function (req, res) {
 
-    });
+});
 
-    app.get('/logout', function (req, res) {
+app.get('/logout', function (req, res) {
 
-    });
+});
 };
